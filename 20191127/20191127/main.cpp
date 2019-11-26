ba27ff3733e61b9eb57dcfb3c94b6403f12450fa
wangze <w18829898295@163.com>
2019-11-26 22:01:36 +0800

    Sobel，Roberts，Canny等算子的边缘检测

--- 20191127/20191127/main.cpp
+++ 20191127/20191127/main.cpp
@@ -14,7 +14,7 @@ int main() {
 	
 	// 图像缩放
 	Mat image11_1;
-	resize(image11, image11_1, Size(image11.cols * 0.2, image11.rows * 0.2), 0, 0, INTER_LINEAR);
+	resize(image11, image11_1, Size(image11.cols / 5, image11.rows / 5), 0, 0, INTER_LINEAR);
 	imshow("image11_1", image11_1);
 
 	// 直方图均衡化
@@ -24,6 +24,14 @@ int main() {
 
 	// 边缘检测
 	Mat image11_3;
+	// Roberts算子
+	//Mat kern = (Mat_<char>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+	//filter2D(image11_2, image11_3, image11_2.depth(), kern);
+	// Sobel算子
+	//Sobel(image11_2, image11_3, );
+	// Prewitt算子
+
+	// Canny算子
 	Canny(image11_2, image11_3, 100, 200);
 	imshow("image11_3", image11_3);
 
