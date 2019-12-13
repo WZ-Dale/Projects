c9622457d04fec9867195f65538625f7861b6162
wangze <w18829898295@163.com>
2019-12-13 17:14:24 +0800

    增加了一些测试

--- Opencv3_Demo/Demo/Demo/main.cpp
+++ Opencv3_Demo/Demo/Demo/main.cpp
@@ -7,20 +7,30 @@
 //#include "E:\Projects\Opencv3_Demo\1_loadDisplaySave_mat_logo\mat.h"
 //#include "E:\Projects\Opencv3_Demo\1_loadDisplaySave_mat_logo\logo.h"
 
-#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\saltImage.h"
-#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\colorReduce.h"
+//#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\saltImage.h"
+//#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\colorReduce.h"
+//#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\contrast.h"
+//#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\addImages.h"
+#include "E:\Projects\Opencv3_Demo\2_saltImage_colorReduce_contrast_addImages_remapping\remapping.h"
 
+#include "E:\Projects\Opencv3_Demo\3_colorDetection_colordetector_extractObject_huesaturation\colorDetection.h"
 
 using namespace std;
 using namespace cv;
 
 int main() {
+// 1
 	//loadDisplaySave_Test();
 	//mat_Test();
 	//logo_Test();
-
+// 2
 	//saltImage_Test();
-	colorReduce_Test();
+	//colorReduce_Test();
+	//contrast_Test();
+	//addImages_Test();
+	//remapping_Test();
+// 3
+	colorDetection_Test();
 
 
 	//destroyAllWindows();
