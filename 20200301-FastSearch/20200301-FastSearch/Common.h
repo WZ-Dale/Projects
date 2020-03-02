5249fc69b201406e13b324b77b63eac5a96390e8
WZ-Dale <w18829898295@163.com>
2020-03-02 22:44:26 +0800

    快速搜索 添加头文件

--- 20200301-FastSearch/20200301-FastSearch/Common.h
+++ 20200301-FastSearch/20200301-FastSearch/Common.h
@@ -1,7 +1,12 @@
 // ¹«¹²Ä£¿é
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include <io.h>
 
 using std::cout;
 using std::endl;
-
+using std::string;
+using std::vector;
