189bf6fda3789e1fc3e2984df3d0de461eaef5f0
WZ-Dale <w18829898295@163.com>
2020-03-03 23:06:09 +0800

    快速搜索 目录列举

--- 20200301-FastSearch/20200301-FastSearch/Common.h
+++ 20200301-FastSearch/20200301-FastSearch/Common.h
@@ -10,3 +10,17 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+
+// 目录列举
+void DirectoryList(const string& path, vector<string>& docs) {
+	_finddata_t file;	//	文件信息结构体
+	const string path_ = path + "\\*.*";	// 该目录下的所有文件
+	intptr_t handle = _findfirst(path_.c_str(), &file);
+	if (handle == -1) {
+		cout << "_findfirst:" << path << "error" << endl;
+		return;
+	}
+	do {
+		docs.push_back(file.name);
+	} while (_findnext(handle, &file) == 0);	//	右兄弟
+}
