d587ff8653109a2a3fff93afa1aafd5d21f49101
WZ-Dale <w18829898295@163.com>
2020-05-06 22:00:00 +0800

    IP相关测试代码 最近这段时间假期浪嗨了，落下了很多，从今天开始继续push

---
+++ File_Sharing/getifaddrs.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(){
+  struct ifaddrs* addrs;
+  getifaddrs(&addrs);
+  while(addrs != NULL){
+    struct sockaddr_in* ip = (struct sockaddr_in*)addrs->ifa_addr;
+    struct sockaddr_in* mask = (struct sockaddr_in*)addrs->ifa_netmask;
+    if(ip->sin_family != AF_INET){
+      addrs = addrs->ifa_next;
+      continue;
+    }
+    if(ip->sin_addr.s_addr == inet_addr("127.0.0.1")){
+      addrs = addrs->ifa_next;
+      continue;
+    }
+    printf("name: %s\n", addrs->ifa_name);
+    printf("ip: %s\n", inet_ntoa(ip->sin_addr));
+    printf("mask: %s\n", inet_ntoa(mask->sin_addr));
+    uint32_t net = ntohl(ip->sin_addr.s_addr & mask->sin_addr.s_addr);
+    uint32_t max_host = ~(ntohl(mask->sin_addr.s_addr));
+    int i = 0;
+    for(; i < max_host; ++i){
+      struct in_addr ip;
+      ip.s_addr = htonl(net + i);
+      printf("ip%d: %s\n", i, inet_ntoa(ip));
+    }
+    
+    addrs = addrs->ifa_next;
+  }
+  return 0;
+}
